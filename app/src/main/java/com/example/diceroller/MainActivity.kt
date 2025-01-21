dcd9ae3232de42ef6d56cbee05a8e10d797697c4
MosheTzelnikerGit <a0583200094@gmail.com>
2025-01-21 16:03:49 +0200

    Completion of the first stage of pressing a button and displaying a cube with a correct number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,25 +2,39 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvDiceResult: TextView = findViewById(R.id.tvDiceResult)
-        val btnRoll: Button = findViewById(R.id.btnRoll)
-        btnRoll.text = "Let's Roll"
+        val tvDiceResult: Button = findViewById(R.id.btnRoll)
 
-
-        btnRoll.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-
-            val randomDiceRoll = (1..6).random()
-            tvDiceResult.text = randomDiceRoll.toString()
+        tvDiceResult.setOnClickListener {
+            rolldice()
         }
+        diceImage = findViewById(R.id.dice_image)
+
+    }
+
+    private fun rolldice() {
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+        Toast.makeText(this, "You rolled a $randomInt!", Toast.LENGTH_SHORT).show()
     }
 }
